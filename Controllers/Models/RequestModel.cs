14b5fb6a00809e96fc4fec28eff643124d92879c
Sanel Hadzini <31168388+theteladras@users.noreply.github.com>
2024-04-19 11:41:28 +0200

    validate request GUID

--- Controllers/Models/RequestModel.cs
+++ Controllers/Models/RequestModel.cs
@@ -10,10 +10,12 @@ public class RequestModel
     public string ProductCode { get; set; }
 
     [Required]
+    [RegularExpression(@"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$", ErrorMessage = "Invalid GUID format.")]
     [JsonPropertyName("tenantId")]
     public string TenantId { get; set; }
 
     [Required]
+    [RegularExpression(@"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$", ErrorMessage = "Invalid GUID format.")]
     [JsonPropertyName("documentId")]
     public string DocumentId { get; set; }
 }
